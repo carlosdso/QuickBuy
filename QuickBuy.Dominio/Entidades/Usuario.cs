0fdfea175353413e7404b34c7eb555066886afd2
Igor Cardoso <igorcardosos@gmail.com>
2020-10-24 14:10:38 -0300

    voltei a propriedade

--- QuickBuy.Dominio/Entidades/Usuario.cs
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -7,7 +7,7 @@ namespace QuickBuy.Dominio.Entidades
         public int Id { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
-
+        public string Nome { get; set; }
         public string SobreNome { get; set; }
 
         /// <summary>
