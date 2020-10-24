7d15e070f5cabbbbc1087e7411fb4cff63e8249d
Igor Cardoso <igorcardosos@gmail.com>
2020-10-24 14:08:17 -0300

    removi propriedade

--- QuickBuy.Dominio/Entidades/Usuario.cs
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -7,12 +7,12 @@ namespace QuickBuy.Dominio.Entidades
         public int Id { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
-        public string Nome { get; set; }
+
         public string SobreNome { get; set; }
 
-/// <summary>
-/// Um Usuario Pode ter nenhum ou muitos pedidos
-/// </summary>
+        /// <summary>
+        /// Um Usuario Pode ter nenhum ou muitos pedidos
+        /// </summary>
         public virtual ICollection<Pedido> Pedidos { get; set; }
 
         public override void Validate()
